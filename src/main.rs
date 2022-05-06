c8f0b2c57af21bdce20e4a99c75b6a9888e74120
Pavle Obradovic <pobradovic@gmail.com>
2022-05-06 20:03:39 +0200

    Output formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
-use std::io::Write;
 use regex::{Regex, Captures};
-use log::{debug};
+use clap::{arg, Command};
+use ansi_term;
+
+const MAX_EXPONENT: usize = 5;
 
 ///
 /// Convert absolute number to base number and exponent.
@@ -10,6 +12,14 @@ use log::{debug};
 /// 1 - kilo, 2 - mega, 3 - giga, etc.
 ///
 fn normalize_units(value: f64, exponent: usize) -> (f64, usize) {
+    if exponent > MAX_EXPONENT {
+        panic!("Provided value is too large");
+    }
+
+    if value <= 0.0 {
+        panic!("Provided value must be greater than 0");
+    }
+
     if value >= 1024.0 {
         return normalize_units(value / 1024.0, exponent + 1);
     }
@@ -19,36 +29,39 @@ fn normalize_units(value: f64, exponent: usize) -> (f64, usize) {
 ///
 /// Return bps with SI prefix based on passed `exponent` value
 ///
-fn print_units(input: (f64, usize)) -> String {
-    let units = ["bps", "kbps", "mbps", "gbps", "tbps", "pbps"];
+fn print_units(input: (f64, usize), unit: &str) -> String {
+    let prefix = ["b", "k", "m", "g", "t", "p"];
     let (value, exponent) = input;
-    format!("{:7.2} {}", value, units[exponent])
+    if exponent > MAX_EXPONENT {
+        panic!("Provided value is too large");
+    }
+    format!("{:7.2} {}{}", value, prefix[exponent], unit)
 }
 
 ///
 /// Parse input and return base value and exponent
-/// Input value should be in format: `1234 mpps`
+/// Input value should be in format: `1234mpps`
 /// Examples:
-/// `1234 pps`, `123 kpps`, `12 mpps`, `60 gbps`
+/// `1234pps`, `123kpps`, `12mpps`, `60gbps`
 ///
-fn parse_units(input: &str) -> (i64, usize) {
+fn parse_units(input: &str) -> (f64, usize) {
     let units = ["k", "m", "g", "t", "p"];
     let mat: Captures;
-    let value: i64;
+    let value: f64;
     let exponent: usize;
 
     // Build regex for matching input argument
-    let re = Regex::new(r"^(\d+)\s*([kmgtp])?([pb])ps(\s)?$").unwrap();
+    let re = Regex::new(r"^(\d+)([kmgtp])?pps(\s)?$").unwrap();
 
 
     // Capture regex groups from input string
     match re.captures(input) {
         Some(v) => mat = v,
-        None => panic!("Format must be in '[0-9]+ [kmgtp]bps'"),
+        None => panic!("Format must be in '[0-9]+[kmgtp]pps'"),
     }
 
     // Fetch and parse base value as f64
-    match mat.get(1).unwrap().as_str().parse::<i64>() {
+    match mat.get(1).unwrap().as_str().parse::<f64>() {
         Ok(v) => value = v,
         Err(_) => panic!("Can't parse input value as float."),
     }
@@ -62,42 +75,196 @@ fn parse_units(input: &str) -> (i64, usize) {
         None => exponent = 0,
     }
 
-    //TODO: differentiate and convert between pps and kbs, based on input
-
     // Return (value, exponent) tuple
     (value, exponent)
 }
 
-fn help() {
-    println!("Convert PPS to BPS.
-Usage examples:
-    > pps-calc 15 mpps
-    > pps-calc 125000 pps")
+///
+/// Calculate bandwidth (bps) for given pps value
+///
+fn to_bps(pps: f64, packet_size: i32) -> f64{
+    pps * packet_size as f64 * 8.0
+}
+
+///
+/// Calculate usable bandwidth coefficient based on provided overhead value.
+/// Overhead value is given in addition to standard ethernet 20B overhead.
+///
+fn get_efficiency(packet_size: i32, overhead: i32) -> f64 {
+    packet_size as f64 / (packet_size + overhead + 20) as f64
+}
+
+fn print_bps_results(pps: f64, exponent: usize, packet_size: i32) {
+
+    let underline = ansi_term::Style::new().underline();
+
+    // Typical packet sizes
+    let mut sizes = vec![64, 594, 1518];
+    if !sizes.contains(&packet_size) {
+        sizes.push(packet_size);
+        sizes.sort();
+    }
+
+    /*
+    Add packet size corresponding to average Simple IMIX packet size.
+    Simple IMIX:
+      64 B x 7 packets =  448 B
+     594 B x 4 packets = 2376 B
+    1518 B x 1 packets = 1518 B
+    -------------------
+            12 packets = 4342 B => ~362 B / packet
+     */
+    sizes.push(362);
+
+    // Tuples with overhead in bytes for most common encapsulations
+    let overheads = [
+        ("VLAN", 4), ("QinQ", 8), ("VXLAN", 50),
+        ("MPLS", 4), ("PPPoE", 8), ("GRE", 4)
+    ];
+
+
+    println!("{:=^60}\n", "[ Packets Per Second Calculator ]");
+
+    println!("Input PPS:  \x1b[1;92m{}\x1b[0m", print_units((pps, exponent), "pps"));
+    println!("Packet size: \x1b[1;92m{} B\x1b[0m\n", packet_size);
+
+    // Throughput section header
+    println!("{:<23}{:<28}{:<28}\x1b[0m",
+             format!("{}", underline.paint("Packet size:")),
+             format!("{}", underline.paint("Throughput:")),
+             format!("{}", underline.paint("With overhead*:"))
+    );
+
+    // Iterate over packet sizes vector and calculate values for each size
+    for (i, p_size) in sizes.iter().enumerate() {
+
+        let normalized = normalize_units(to_bps(pps, *p_size), exponent);
+        let efficiency = get_efficiency(*p_size, 0);
+        let normalized_o = normalize_units(to_bps(pps * efficiency, *p_size), exponent);
+
+        // Color last row (IMIX traffic) result
+        if i == sizes.len() - 1 { print!("\x1b[1;96m"); }
+
+        println!("{}{:<15}{:<20}{:<20}\x1b[0m",
+                 // Color user selected packet size row
+                 if *p_size == packet_size { "\x1b[1m\x1b[97m" } else { "" }, //Color user selected packet size
+                 // Print text description for IMIX instead of packet size
+                 if i == sizes.len() - 1 { format!("{}", "Simple IMIX") } else { format!("{:5} B", p_size) },
+                 print_units(normalized, "bps"),
+                 format!("{} ({:.2}%)", print_units(normalized_o, "bps"), efficiency * 100.0)
+        );
+    }
+
+    println!("\n\nApproximate theoretical ethernet throughputs with common\nencapsulations overheads:\n");
+
+    // Overhead section header
+    println!("{:<23}{:<28}{:<28}\x1b[0m",
+             format!("{}", underline.paint("Encap:")),
+             format!("{}", underline.paint("Overhead:")),
+             format!("{}", underline.paint("With overhead*:"))
+    );
+
+    for (tech, overhead) in overheads {
+        let efficiency = get_efficiency(packet_size, overhead);
+        let normalized_o = normalize_units(to_bps(pps * efficiency, packet_size), exponent);
+        println!("{:<15}{:<20}{:<20}\x1b[0m",
+                 tech, format!("{:3} B", overhead),
+                 format!("{} ({:.2}%)", print_units(normalized_o, "bps"), efficiency * 100.0)
+        );
+    }
+
+    println!("\n\n* These are just approximations\n  and are \x1b[1;97mnot\x1b[0m accurate calculations.");
+
+    println!("\n{:=^60}", "");
 }
 
 fn main() -> io::Result<()> {
+    let arguments = Command::new("Packets Per Second calculator")
+        .version("0.2")
+        .author("Pavle Obradovic <pobradovic08@gmail.com>")
+        .about("Converts PPS value to maximum theoretical throughput.")
+        .arg(arg!(<pps> "Packets Per Second in [pps, kpps, mpps, ...]"))
+        .arg(arg!(-s --size [packet_size] "Packet size in bytes").default_value("64"))
+        .get_matches();
 
-    // Multiply packets per second with packet size (in bits) to get base bps
-    let to_bps = |pps: i64, packet_size: i64| pps * packet_size * 8;
+    let pps_input: &str;
+    let packet_size: i32;
 
-    // String buffer for user input
-    let mut line = String::new();
-
-    // Read user input
-    // TODO: move to argv
-    print!("Input PPS: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut line).unwrap();
-
-    // Parse user input
-    let (pps, exponent) = parse_units(&line);
-
-    // Print maximum bps for multiple packet sizes
-    for packet_size in [64, 578, 1500] {
-        println!("{} for {:4}B packets",
-                 print_units(normalize_units(to_bps(pps, packet_size) as f64, exponent)),
-                 packet_size);
+    match arguments.value_of("pps") {
+        Some(v) => pps_input = v,
+        None => panic!("Error parsing pps value"),
     }
 
+    match arguments.value_of("size") {
+        Some(v) => match v.parse::<i32>() {
+            Ok(v) => match v {
+                64..=65536 => packet_size= v,
+                _ => panic!("Packet size must be between 64B and 64KB (65536B)")
+            },
+            Err(_) => packet_size = 64,
+        },
+        None => packet_size = 64,
+    }
+
+    let (pps, exponent) = parse_units(pps_input);
+    print_bps_results(pps, exponent, packet_size);
+
     Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_unit_normalization() {
+        assert_eq!(normalize_units(1.0, 0), (1.0, 0));
+        assert_eq!(normalize_units(1023.0, 0), (1023.0, 0));
+        assert_eq!(normalize_units(1024.0, 0), (1.0, 1));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_unit_normalization_out_of_bounds() {
+        normalize_units(1024.0, 5);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_unit_normalization_zero() {
+        normalize_units(0.0, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_unit_normalization_negative() {
+        normalize_units(-1.0, 0);
+    }
+
+    #[test]
+    fn test_print_units() {
+        assert_eq!(print_units((1.0, 0), "bps"), "   1.00 bps");
+        assert_eq!(print_units((512.0, 0), "bps"), " 512.00 bps");
+        assert_eq!(print_units((1023.0, 0), "bps"), "1023.00 bps");
+
+        assert_eq!(print_units((0.001, 0), "bps"), "   0.00 bps");
+        assert_eq!(print_units((1023.1234, 0), "bps"), "1023.12 bps");
+        assert_eq!(print_units((1023.99, 0), "bps"), "1023.99 bps");
+        assert_eq!(print_units((1023.9999, 0), "bps"), "1024.00 bps");
+
+        assert_eq!(print_units((1.0, 0), "bps"), "   1.00 bps");
+        assert_eq!(print_units((1.0, 0), "bps"), "   1.00 bps");
+        assert_eq!(print_units((1.0, 1), "bps"), "   1.00 kbps");
+        assert_eq!(print_units((1.0, 2), "bps"), "   1.00 mbps");
+        assert_eq!(print_units((1.0, 3), "bps"), "   1.00 gbps");
+        assert_eq!(print_units((1.0, 4), "bps"), "   1.00 tbps");
+        assert_eq!(print_units((1.0, 5), "bps"), "   1.00 pbps");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_print_units_out_of_bounds() {
+        print_units((1.0, 6), "bps");
+    }
 }
